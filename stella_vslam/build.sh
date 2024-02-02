c424710b08a2e54646c2bc86527acaf9ea459c10
Yiwei Gong <imwithye@gmail.com>
2024-02-02 04:34:06 +0000

    remove folder

--- stella_vslam/build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-export SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd $SCRIPT_DIR
-
-git clone https://github.com/stella-cv/stella_vslam.git && \
-    cd stella_vslam && git checkout a18add7
-docker build -t stella_vslam -f Dockerfile.socket .
